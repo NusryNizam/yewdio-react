7d787d181eeaa623f721bf77e6f366eef94cd8f9
Nusry Nizam <nusrynizam@gmail.com>
2023-06-08 22:49:57 +0530

    added icon and pwa support

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,77 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { VitePWA, VitePWAOptions } from "vite-plugin-pwa";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+
+const manifestForPlugin: Partial<VitePWAOptions> = {
+  registerType: "prompt",
+  includeAssets: ["favicon.ico", "apple-touch-icon.png", "masked-icon.svg"],
+  manifest: {
+    name: "Yewdio",
+    short_name: "Yewdio",
+    description:
+      "Listen to YouTube as audio on-the-go.",
+    theme_color: "#0094ff",
+    background_color: "#0094ff",
+    categories: ["entertainment", "music"],
+    display: "standalone",
+    id: "/",
+    scope: "/",
+    start_url: "/",
+    orientation: "portrait",
+    icons: [
+      {
+        src: "./src/assets/icons/icon-72x72.png",
+        sizes: "72x72",
+        type: "image/png",
+        purpose: "maskable any",
+      },
+      {
+        src: "./src/assets/icons/icon-96x96.png",
+        sizes: "96x96",
+        type: "image/png",
+        purpose: "maskable any",
+      },
+      {
+        src: "./src/assets/icons/icon-128x128.png",
+        sizes: "128x128",
+        type: "image/png",
+        purpose: "maskable any",
+      },
+      {
+        src: "./src/assets/icons/icon-144x144.png",
+        sizes: "144x144",
+        type: "image/png",
+        purpose: "maskable any",
+      },
+      {
+        src: "./src/assets/icons/icon-152x152.png",
+        sizes: "152x152",
+        type: "image/png",
+        purpose: "maskable any",
+      },
+      {
+        src: "./src/assets/icons/icon-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+        purpose: "maskable any",
+      },
+      {
+        src: "./src/assets/icons/icon-384x384.png",
+        sizes: "384x384",
+        type: "image/png",
+        purpose: "maskable any",
+      },
+      {
+        src: "./src/assets/icons/icon-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "maskable any",
+      },
+    ],
+  },
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), VitePWA(manifestForPlugin)],
+});
